6bcf2bd802f88db3a4a179a12f7c99735cd0568b
Amymaara <98781672+Amymaara@users.noreply.github.com>
2026-03-28 09:52:10 +0200

    grow stages work

--- Farming Game/Assets/Scripts/CropManager.cs
+++ Farming Game/Assets/Scripts/CropManager.cs
@@ -7,13 +7,47 @@ public class CropManager : MonoBehaviour
     public Tilemap cropTilemap;
     public FarmManager farmManager;
 
-    private Dictionary<Vector3Int, CropData> plantedCrops = new Dictionary<Vector3Int, CropData>();
+    private Dictionary<Vector3Int, PlantedCrop> plantedCrops = new Dictionary<Vector3Int, PlantedCrop>();
 
+    private void Update()
+    {
+        UpdateGrowth();
+    }
     public void PlantCrop(Vector3Int cellPos, CropData cropData)
     {
-        cropTilemap.SetTile(cellPos, cropData.growthStageTiles[0]);
-        plantedCrops[cellPos] = cropData;
+        if (cropTilemap.GetTile(cellPos) != null) return;
 
+        cropTilemap.SetTile(cellPos, cropData.growthStageTiles[0]);
+
+        PlantedCrop newCrop = new PlantedCrop
+        {
+            cropData = cropData,
+            currentStage = 0,
+            timer = cropData.growthStageDurations[0]
+        };
+
+        plantedCrops[cellPos] = newCrop;
     }
 
+    private void UpdateGrowth()
+    {
+        foreach (var pair in plantedCrops)
+        {
+            Vector3Int pos = pair.Key;
+            PlantedCrop crop = pair.Value;
+
+            if(crop.currentStage >= crop.cropData.growthStageTiles.Length -1)
+                continue;
+
+            crop.timer -= Time.deltaTime;
+
+            if (crop.timer <= 0f)
+            {
+                crop.currentStage++;
+                cropTilemap.SetTile(pos, crop.cropData.growthStageTiles[crop.currentStage]);
+                crop.timer = crop.cropData.growthStageDurations[crop.currentStage];
+
+            }
+        }
+    }
 }
---
+++ Farming Game/Assets/Scripts/PlantedCrop.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[System.Serializable]
+public class PlantedCrop : MonoBehaviour
+{
+    public CropData cropData;
+    public int currentStage;
+    public float timer;
+}
